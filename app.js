8fd138d866408ec2665e7582795d80ba78d15ea6
bilal programming <bilaltechnical93@gmail.com>
2025-02-21 19:24:14 +0500

    Basic Node js Tut

---
+++ app.js
@@ -0,0 +1,9 @@
+// export in another file
+module.exports={
+    a:10,
+    b:30,
+    z:function(){
+        return ' hi, i cmoes from app.js/...'
+
+    }
+}
---
+++ index.js
@@ -0,0 +1,33 @@
+  //======= for loop and if-else in js=======
+// for (i=0;i<10;i++){
+//     if(i%2===0){
+//         console.log(i," is Even")
+
+//     }
+    
+// } 
+
+   // ========arr in js============
+//   const arr=[12,35,67,8,43]
+//   len=arr.length
+//   for(i=0;i<len;i++){
+//     console.log(i)
+//   }  
+
+// ========filter in js============
+// let arr=[3,20,100,45]
+// filter=arr.filter((n)=> n>10)
+// console.log(filter)
+
+
+  //==== import something fron another file=======
+//  const app=require("./app")
+// console.log(app)
+
+           //===Modules===========  the feautures that programming language provides like console.log in js
+
+const fs=require("fs").writeFileSync // instead of all fs used only those needed
+fs("hello3.txt","Hi iam new file")         
+console.log(__dirname)    
+console.log(__filename)       
+  
