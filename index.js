3a6100681e2c5f608b5dfb8263b34293bedc50a0
bilal programming <bilaltechnical93@gmail.com>
2025-02-23 01:36:43 +0500

    create server

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+  // // =====Normal Function
+  // function test(a){
+  //   return a*10
+  // }
+  // //===== Arrow Function
+  // const test=(a)=> a*10
+
+  
+  
+  
   //======= for loop and if-else in js=======
 // for (i=0;i<10;i++){
 //     if(i%2===0){
@@ -26,8 +36,19 @@
 
            //===Modules===========  the feautures that programming language provides like console.log in js
 
-const fs=require("fs").writeFileSync // instead of all fs used only those needed
-fs("hello3.txt","Hi iam new file")         
-console.log(__dirname)    
-console.log(__filename)       
+// const fs=require("fs").writeFileSync // instead of all fs used only those needed
+// fs("hello3.txt","Hi iam new file")         
+// console.log(__dirname)    
+// console.log(__filename)       
+
+//================  Make server in node js==================
+    // http is module which handle request and response of server
+ const http=require('http')
   
+ http.createServer( (req,resp)=>{
+  resp.write("Hi Bilal.......")
+  resp.end()
+ }
+ ).listen(1000)
+
+ 
