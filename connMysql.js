7a09a8d3bbbec5996b17e5b4df32611ebea2424b
bilal programming <bilaltechnical93@gmail.com>
2025-03-25 01:28:06 +0500

    connect node with mysql

---
+++ connMysql.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql")
+const con = mysql.createConnection(
+    {
+        host:"localhost",
+        user:"root",
+        password:"",
+        database:"employee_db"
+
+    }
+)
+module.exports=con
+
+
+
--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-  // // =====Normal Function
-  // function test(a){
-  //   return a*10
-  // }
-  // //===== Arrow Function
-  // const test=(a)=> a*10
+// // =====Normal Function
+// function test(a){
+//   return a*10
+// }
+// //===== Arrow Function
+// const test=(a)=> a*10
 
-  
-  
-  
-  //======= for loop and if-else in js=======
+
+
+
+//======= for loop and if-else in js=======
 // for (i=0;i<10;i++){
 //     if(i%2===0){
 //         console.log(i," is Even")
 
 //     }
-    
+
 // } 
 
-   // ========arr in js============
+// ========arr in js============
 //   const arr=[12,35,67,8,43]
 //   len=arr.length
 //   for(i=0;i<len;i++){
@@ -30,11 +30,11 @@
 // console.log(filter)
 
 
-  //==== import something fron another file=======
+//==== import something fron another file=======
 //  const app=require("./app")
 // console.log(app)
 
-           //===Modules===========  the feautures that programming language provides like console.log in js
+//===Modules===========  the feautures that programming language provides like console.log in js
 
 // const fs=require("fs").writeFileSync // instead of all fs used only those needed
 // fs("hello3.txt","Hi iam new file")         
@@ -42,9 +42,9 @@
 // console.log(__filename)       
 
 //================  Make server in node js==================
-    
+
 //  const http=require('http')  // http is module which handle request and response of server
-  
+
 //  http.createServer( (req,resp)=>{
 //   resp.write("Hi Bilal.......")
 //   resp.end()
@@ -52,15 +52,15 @@
 //  ).listen(1000)
 
 
-   //===== used packages
-  
+//===== used packages
+
 // import color from'colors'
 // console.log("package.json".green)
-  
+
 // import chalk from 'chalk';
 // console.log(chalk.red('Hello world!'));
 // console.log("Node js tutorial...")
-    
+
 
 //================= Make API============================================
 //  const http=require('http') // http is mudule 
@@ -106,10 +106,10 @@
 //     console.log(data)
 
 //   })
-  
+
 
 // })
-  //==================crud operation on file=================
+//==================crud operation on file=================
 // const fs=require("fs")  
 // const path=require("path")
 // const pathDir=path.join(__dirname,'crud')
@@ -172,14 +172,14 @@
 // app.get('',(req,resp)=>{   // get is method which   provides routes
 //   console.log("data sent by browser",req.query.name)
 //   resp.send("Welcome "+  req.query.name)
- 
-  
+
+
 // })  
 
 // // contact page
 // app.get('/contact',(req,resp)=>{
 //   resp.send("<h2> Welcome user Contact Page</h2><br><p>Contact:03112061159</p>")
-  
+
 // })
 
 // // about page
@@ -228,7 +228,7 @@
 // const express=require("express")
 // const path=require("path")
 // const app=express()
- 
+
 // const publicPath=path.join(__dirname,'public')
 
 // app.use(express.static(publicPath))// function of express(middleware)
@@ -258,7 +258,7 @@
 // })
 // app.listen(5000)
 
-  
+
 //=========  template engine is used for dynamic page====
 // const express=require("express")
 // const app=express()
@@ -283,7 +283,7 @@
 
 
 // ==middleware=== is used with routes,through this we can access request,response and modify them
- // it is used for authenication
+// it is used for authenication
 
 // const express=require("express")
 // const app=express() 
@@ -314,7 +314,7 @@
 
 // app.listen(300)
 
- //======route level middleware========
+//======route level middleware========
 
 // const express=require("express")
 // const app=express()
@@ -355,7 +355,7 @@
 
 // getData()
 
-  
+
 // db conncect with separate files
 //const dbConect=require("./mongodb")
 //handle promise
@@ -375,7 +375,7 @@
 // }
 // getdata()
 
-  // connect node with mongo using Mongoose(is a package which connect node with mongo db)
+// connect node with mongo using Mongoose(is a package which connect node with mongo db)
 
 // const mongoose=require("mongoose")
 // const main=async ()=>{
@@ -447,19 +447,277 @@
 // }
 // findInDb()
 
- 
+
 // POST API WITH MONGOOSE
-const express=require("express")
-require("./config")
-const deptModel=require("./dept")
+// const express = require("express")
+// require("./config")
+// const deptModel = require("./dept")
 
-const app=express()
+// const app = express()
+// app.use(express.json())
+
+// app.post("/create", async (req, resp) => {
+//   let data = new deptModel(req.body)
+//   let result = await data.save()
+//   console.log(result)
+//   resp.send(result)
+// })
+
+// //PUT API WITH MONGOOSE
+// app.put("/create/:_id", async (req, resp) => {
+//   let data = await deptModel.updateOne(
+//     req.params,
+//     {
+//       $set: req.body
+//     }
+//   )
+
+//   resp.send(data)
+// })
+
+// //GET API  WITH MONGOOSE
+// app.get("/create", async (req, resp) => {
+//   const data = await deptModel.find({ ename: "Bilal" })
+//   console.log(data)
+//   resp.send(data)
+// })
+
+// //DELETE API (using _ID in query param) WITH MONGOOSE
+// app.delete("/delete/:_id", async (req, resp) => {
+//   let data = await deptModel.deleteOne(req.params)
+//   console.log(req.params)
+//   resp.send(data)
+// })
+
+// app.listen(4000)
+
+//====SEARCH API=======
+// const express=require("express")
+// require("./config")
+// const deptModel=require("./dept")
+
+// const app=express()
+
+// app.get("/search/:key",async (req,resp)=>{
+//   const data=await deptModel.find( //used regex for search
+//     {
+//       "$or":[
+//         {"ename":{$regex:req.params.key}},
+//         {"deptName":{$regex:req.params.key}}
+//       ]
+//     }
+//   )
+//   console.log(data)
+//   resp.send(data)
+// })
+// app.listen(5000)
+
+//==UPLOAD FILE USING API(MULTER)=====
+// const express = require("express")
+// const multer = require("multer")
+// const app = express()
+
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, "public")// where to upload file
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, file.fieldname + "-" + Date.now() + ".txt")
+
+
+//     }
+
+//   })
+
+// }).single("user_file")
+
+// app.post("/upload",upload, (req, resp) => {
+//   resp.send("file uploaded.")
+// })
+// app.listen(6000)
+
+//OS Modules
+// const os=require('os')
+// console.log(os.freemem()/(1024*1024*1024))  // free ram in system
+// console.log(os.totalmem()/(1024*1024*1024))
+// console.log(os.hostname())
+// console.log(os.platform())
+// console.log(os.userInfo())
+// console.log(os.arch())
+
+//===event and event emitter==
+// const express=require("express")
+// const EventEmitter=require("events") //Import EventEmitter
+// const app=express()
+// const event=new EventEmitter() //make object of eventemitter
+
+// let count=0
+// //handle event
+// event.on("countApi",()=>{
+//   count++
+//   console.log(count,"event called")
+// })
+// app.get("/",(req,resp)=>{
+//   resp.send(" api called")
+//   event.emit("countApi") //generate event
+// })
+
+// app.get("/search",(req,resp)=>{
+//   resp.send("search api called")
+//   event.emit("countApi")
+// })
+
+// app.get("/update",(req,resp)=>{
+//   resp.send("updated api called")
+//   event.emit("countApi")
+// })
+// app.listen(5000)
+
+// ===CONNECT WITH MYSQL=======
+// const mysql = require("mysql")
+
+// const con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "testdb"
+// })
+// con.connect((err) => {   // check connect with my sql or not
+//   if (!err) {
+//     console.log("connect with mysql")
+//   }
+//   else {
+//     console.log("error")
+//   }
+// })
+
+// con.query("select * from emp", (err, data) => {  //run query
+//   if (!err) {
+//     console.log(data)
+//   }
+
+// })
+
+
+// GET API WITH MYSQL
+// const express=require("express")
+// const connectdb=require("./connMysql")
+// const { error } = require("simple-logs")
+// const app=express()
+// app.use(express.json())
+
+
+// app.get("/", (req,resp)=>{
+//   const data=connectdb.query("select * from emp",(err,data)=>{
+//     if(!err){
+//     resp.send(data)
+//     }
+//     else{
+//       resp.send("error")
+//     }
+//   }) 
+// })
+
+// //POST API MY SQL
+
+// app.post("/",(req,resp)=>{
+//   connectdb.query("insert into emp set ?",req.body,(err,result,fields)=>{
+//     if(err) throw error //throw error
+//     else{
+//       resp.send(result)
+//     }
+//   })
+
+
+// })
+
+// //PUT API MY SQL
+
+// app.put("/:id",(req,resp)=>{
+//   const data=[req.body.ename,req.body.deptid,req.params.id]
+//  const result= connectdb.query("update emp set ename= ?,deptid=? where id=?",data,(err,result,fields)=>{
+//     if (err) throw error
+//     else{
+//       resp.send(result)
+//     }
+//   })
+// })
+
+// app.delete("/:id",(req,resp)=>{
+//   connectdb.query("delete from emp where id = ?",req.params.id,(err,result)=>{
+//     if(err) throw error
+//     else{
+//       resp.send(result)
+
+//     }
+
+//   })
+
+// })
+// app.listen(4000)
+
+
+//PRACTICE
+
+const express = require("express")
+const dbConect = require("./connMysql")
+const { error } = require("simple-logs")
+const app = express()
 app.use(express.json())
+//get api for  Fetch all employees.
+app.get("/", (req, resp) => {
+  dbConect.query("select * from emp", (err, result) => {
+    if (err) throw error
+    else {
+      resp.status(200).send({ status: 'success', data: result });    }
 
-app.post("/",async (req,resp)=>{
-  let data=new deptModel(req.body)
-  let result=await data.save()
-  console.log(result)
-  resp.send(result)
+  })
+
 })
-app.listen(4000)
+
+//get api for  Fetch employee by their id.
+app.get("/searchById/:id", (req, resp) => {
+  dbConect.query("select * from emp where id = ?", req.params.id, (err, result) => {
+    if (err) throw error
+    else {
+      resp.send(result)
+    }
+
+  })
+
+})
+//post api for add new emp
+app.post("/addEmp", (req, resp) => {
+
+  dbConect.query("insert into emp set ?",req.body,(err,result,fields)=>{
+    if(err) throw error
+    else{
+      resp.send(result)
+    }
+  })
+
+})
+
+//put api(update emp by their id)
+app.put("/update/:id",(req,resp)=>{
+  const emp=[req.body.salary,req.body.name,req.params.id]
+  dbConect.query("UPDATE EMP SET salary =?,name =? WHERE id=?",emp,(err,result)=>{
+    if(err ) throw error
+    else{
+      resp.send(result)
+    }
+  })
+
+})  
+
+//delete api  Delete an employee by their id.
+app.delete("/delete/:id",(req,resp)=>{
+  dbConect.query("delete from emp where id=?",req.params.id,(err,result)=>{
+    if(err) throw error
+    else{
+      resp.send(result)
+    }
+  })
+})
+app.listen(5000)
